6debe08a3c7cfcf8fc9df3d1d9b777b09d19737c
cacutler <calexcutler@gmail.com>
2026-07-08 14:57:41 -0600

    Added some type annotations

--- pyserver/session_store.py
+++ pyserver/session_store.py
@@ -1,17 +1,17 @@
 import os, base64
 class SessionStore:
-    def __init__(self): #initialize our data
+    def __init__(self) -> None: #initialize our data
         self._sessionData = {}
         return
-    def generateSessionId(self): #generate a large, random number for the session ID
+    def generateSessionId(self) -> str: #generate a large, random number for the session ID
         rnum = os.urandom(32)
         rstr = base64.b64encode(rnum).decode("utf-8")
         return rstr
-    def createSession(self):
+    def createSession(self) -> str:
         sessionId = self.generateSessionId() #make a new session ID
         self._sessionData[sessionId] = {} #add a new session to the session store
         return sessionId
-    def getSession(self, sessionId): #retrieve an existing session from the session store
+    def getSession(self, sessionId: str) -> dict | None: #retrieve an existing session from the session store
         if sessionId in self._sessionData:
             return self._sessionData[sessionId]
         else:
